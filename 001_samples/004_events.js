79af7034f8ce7e21f7deb01af32cde1c966ceaca
irapanch <117355580+irapanch@users.noreply.github.com>
2024-07-25 16:19:09 +0300

    add event

--- 001_samples/004_events.js
+++ 001_samples/004_events.js
@@ -5,4 +5,8 @@ const customEmitter = new EventEmitter();
 customEmitter.on("greet", (name) => {
   console.log(`Hello ${name}!`);
 });
+customEmitter.on("good", (name) => {
+  console.log(`Hello ${name}!`);
+});
 customEmitter.emit("greet", "Ira");
+customEmitter.emit("good", "Roma");
